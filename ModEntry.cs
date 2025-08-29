278e2e5241bbb336482818c897d14e733ac72e63
JyGein <52318346+JyGein@users.noreply.github.com>
2025-08-29 19:34:44 -0400

    tried to fix destiny's bug

--- ModEntry.cs
+++ ModEntry.cs
@@ -21,7 +21,7 @@ public sealed class Elestrals : SimpleMod
 {
     internal static Elestrals Instance { get; private set; } = null!;
     internal Harmony Harmony { get; }
-    internal DuoApis DuoApis { get; }
+    internal DuoApis DuoApis { get; private set; } = null!;
     public ApiImplementation ApiImplementation { get; }
     internal IKokoroApi KokoroApi { get; }
     internal IKokoroApi.IV2 KokoroApiV2 { get; }
@@ -144,22 +144,17 @@ public sealed class Elestrals : SimpleMod
     ];
     internal static IEnumerable<Type> Equilynx_AllArtifact_Types
         => Equilynx_CommonArtifact_Types
-        .Concat(Equilynx_BossArtifact_Types)
-        .Concat(Equilynx_DuoArtifact_Types);
+        .Concat(Equilynx_BossArtifact_Types);
 
 
     public Elestrals(IPluginPackage<IModManifest> package, IModHelper helper, ILogger logger) : base(package, helper, logger)
     {
         Instance = this;
 
-        /* We use Kokoro to handle our statuses. This means Kokoro is a Dependency, and our mod will fail to work without it.
-         * We take from Kokoro what we need and put in our own project. Head to ExternalAPI/StatusLogicHook.cs if you're interested in what, exactly, we use.
-         * If you're interested in more fancy stuff, make sure to peek at the Kokoro repository found online. */
         KokoroApi = helper.ModRegistry.GetApi<IKokoroApi>("Shockah.Kokoro")!;
         KokoroApiV2 = KokoroApi.V2;
         EnergyApi = helper.ModRegistry.GetApi<IEnergyApi>("JyGein.Energy")!;
         Harmony = new(package.Manifest.UniqueName);
-        DuoApis = new(helper);
         ApiImplementation = new ApiImplementation();
         _ = new CardScalingManager();
         _ = new EarthStoneDepositManager();
@@ -175,12 +170,6 @@ public sealed class Elestrals : SimpleMod
         RandomDroneMoveLocaleFix.ApplyPatches(Harmony);
         NegativeStatusManager.ApplyPatches(Harmony);
 
-        /* These localizations lists help us organize our mod's text and messages by language.
-         * For general use, prefer AnyLocalizations, as that will provide an easier time to potential localization submods that are made for your mod 
-         * IMPORTANT: These localizations are found in the i18n folder (short for internationalization). The Demo Mod comes with a barebones en.json localization file that you might want to check out before continuing 
-         * Whenever you add a card, artifact, character, ship, pretty much whatever, you will want to update your locale file in i18n with the necessary information
-         * Example: You added your own character, you will want to create an appropiate entry in the i18n file. 
-         * If you would rather use simple strings whenever possible, that's also an option -you do you. */
         AnyLocalizations = new JsonLocalizationProvider(
             tokenExtractor: new SimpleLocalizationTokenExtractor(),
             localeStreamFunction: locale => package.PackageRoot.GetRelativeFile($"i18n/{locale}.json").OpenRead()
@@ -189,8 +178,6 @@ public sealed class Elestrals : SimpleMod
             new CurrentLocaleOrEnglishLocalizationProvider<IReadOnlyList<string>>(AnyLocalizations)
         );
 
-        /* Assigning our ISpriteEntry objects manually. This is the easiest way to do it when starting out!
-         * Of note: GetRelativeFile is case sensitive. Double check you've written the file names correctly */
         Equilynx_Character_DefaultCardBackground = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/cards/equilynx/defaultcardbackground.png"));
         Equilynx_Character_NexusCardBackground = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/cards/equilynx/NexusTemp.png"));
         Equilynx_Character_CardFrame = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/characters/equilynx/character_cardframe.png"));
@@ -225,54 +212,28 @@ public sealed class Elestrals : SimpleMod
         DefaultDuoArtifactSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/artifacts/Duos/DefaultSprite.png"));
         DefaultInactiveDuoArtifactSprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/artifacts/Duos/DefaultInactiveSprite.png"));
 
-        /* Decks are assigned separate of the character. This is because the game has decks like Trash which is not related to a playable character
-         * Do note that Color accepts a HEX string format (like Color("a1b2c3")) or a Float RGB format (like Color(0.63, 0.7, 0.76). It does NOT allow a traditional RGB format (Meaning Color(161, 178, 195) will NOT work) */
         Equilynx_Deck = helper.Content.Decks.RegisterDeck("EquilynxDeck", new DeckConfiguration()
         {
             Definition = new DeckDef()
             {
-                /* This color is used in various situations. 
-                 * 
-                 * It is used as the deck's rarity 'shine'
-                 * If a playable character uses this deck, the character Name will use this color
-                 * If a playable character uses this deck, the character mini panel will use this color */
                 color = new Color("254d0c"),
 
-                /* This color is for the card name in-game
-                 * Make sure it has a good contrast against the CardFrame, and take rarity 'shine' into account as well */
-                //titleColor = new Color("9BF200")
                 titleColor = new Color("FFFFFF")
             },
-            /* We give it a default art and border some Sprite types by adding '.Sprite' at the end of the ISpriteEntry definitions we made above. */
             DefaultCardArt = Equilynx_Character_DefaultCardBackground.Sprite,
             BorderSprite = Equilynx_Character_CardFrame.Sprite,
 
-            /* Since this deck will be used by our Demo Character, we'll use their name. */
             Name = AnyLocalizations.Bind(["character", "Equilynx", "name"]).Localize,
 
             ShineColorOverride = (shineColorOverrideArgs) => shineColorOverrideArgs.DefaultShineColor.gain(0.5)
         });
 
-
-        /* Let's create some animations, because if you were to boot up this mod from what you have above,
-         * DemoCharacter would be a blank void inside a box, we haven't added their sprites yet! 
-         * We first begin by registering the animations. I know, weird. 'Why are we making animations when we still haven't made the character itself', stick with me, okay? 
-         * Animations are actually assigned to Deck types, not Characters! */
-
-        /*Of Note: You may notice we aren't assigning these ICharacterAnimationEntry and ICharacterEntry to any object, unlike stuff above,
-        * It's totally fine to assign them if you'd like, but we don't have a reason to so in this mod */
         helper.Content.Characters.V2.RegisterCharacterAnimation(new CharacterAnimationConfigurationV2()
         {
-            /* What we registered above was an IDeckEntry object, but when you register a character animation the Helper will ask for you to provide its Deck 'id'
-             * This is simple enough, as you can get it from DemoMod_Deck */
             CharacterType = Equilynx_Deck.UniqueName,
 
-            /* The Looptag is the 'name' of the animation. When making shouts and events, and you want your character to show emotions, the LoopTag is what you want
-             * In vanilla Cobalt Core, there are 4 'animations' looptags that any character should have: "neutral", "mini", "squint" and "gameover",
-             * as these are used in: Neutral is used as default, mini is used in character select and out-of-combat UI, Squink is hardcoded used in certain events, and Gameover is used when your run ends */
             LoopTag = "neutral",
 
-            /* The game doesn't use frames properly when there are only 2 or 3 frames. If you want a proper animation, avoid only adding 2 or 3 frames to it */
             Frames = new[]
             {
                 Equilynx_Character_Neutral_0.Sprite,
@@ -287,7 +248,6 @@ public sealed class Elestrals : SimpleMod
             LoopTag = "mini",
             Frames = new[]
             {
-                /* Mini only needs one sprite. We call it animation just because we add it the same way as other expressions. */
                 Equilynx_Character_Mini_0.Sprite
             }
         });
@@ -304,8 +264,6 @@ public sealed class Elestrals : SimpleMod
             }
         });
 
-        /* Wait, so if we want 'gameover', why doesn't this demo come with the registration for it?
-         * Answer: You should be able to use the knowledge you have earned so far to register your own animations! If you'd like, try making the 'gameover' registration code here. You can use whatever sprite you want */
         helper.Content.Characters.V2.RegisterCharacterAnimation(new CharacterAnimationConfigurationV2()
         {
             CharacterType = Equilynx_Deck.UniqueName,
@@ -315,14 +273,10 @@ public sealed class Elestrals : SimpleMod
                 Equilynx_Character_Gameover_0.Sprite
             }
         });
-        /* Let's continue with the character creation and finally, actually, register the character! */
         helper.Content.Characters.V2.RegisterPlayableCharacter("Equilynx", new PlayableCharacterConfigurationV2()
         {
-            /* Same as animations, we want to provide the appropiate Deck type */
             Deck = Equilynx_Deck.Deck,
 
-            /* The Starter Card Types are, as the name implies, the cards you will start a DemoCharacter run with. 
-             * You could provide vanilla cards if you want, but it's way more fun to create your own cards! */
             Starters = new()
             {
                 cards = [
@@ -331,12 +285,8 @@ public sealed class Elestrals : SimpleMod
                 ]
             },
 
-            /* This is the little blurb that appears when you hover over the character in-game.
-             * You can make it fluff, use it as a way to tell players about the character's playstyle, or a little bit of both! */
             Description = AnyLocalizations.Bind(["character", "Equilynx", "description"]).Localize,
 
-            /* This is the fancy panel that encapsulates your character while in active combat.
-             * It's recommended that it follows the same color scheme as the character and deck, for cohesion */
             BorderSprite = Equilynx_Character_Panel.Sprite,
             ExeCardType = typeof(EquilynxExeCard)
         });
@@ -351,150 +301,12 @@ public sealed class Elestrals : SimpleMod
                 ]
             }
         );
-        /* The basics for a Character mod are done!
-         * But you may still have mechanics you want to tackle, such as,
-         * 1. How to make cards
-         * 2. How to make artifacts
-         * 3. How to make ships
-         * 4. How to make statuses */
-
-        /* 1. CARDS
-         * Elestrals comes with a neat folder called Cards where all the .cs files for our cards are stored. Take a look.
-         * You can decide to not use the folder, or to add more folders to further organize your cards. That is up to you.
-         * We do recommend keeping files organized, however. It's way easier to traverse a project when the paths are clear and meaningful */
-
-        /* Here we register our cards so we can find them in game.
-         * Notice the IDemoCard interface, you can find it in InternalInterfaces.cs
-         * Each card in the IEnumerable 'DemoMod_AllCard_Types' will be asked to run their 'Register' method. Open a card's .cs file, and see what it does 
-         * We *can* instead register characts one by one, like what we did with the sprites. If you'd like an example of what that looks like, check out the Randall mod by Arin! */
         foreach (var cardType in Elestrals_AllCard_Types)
             AccessTools.DeclaredMethod(cardType, nameof(IElestralsCard.Register))?.Invoke(null, [helper]);
 
-        /* 2. ARTIFACTS
-         * Creating artifacts is pretty similar to creating Cards
-         * Take a look at the Artifacts folder for demo artifacts!
-         * You may also notice we're using the other interface from InternalInterfaces.cs, IDemoArtifact, to help us out */
         foreach (var artifactType in Equilynx_AllArtifact_Types)
             AccessTools.DeclaredMethod(artifactType, nameof(IElestralsArtifact.Register))?.Invoke(null, [helper]);
 
-        /* 3. SHIPS
-         * Creating a ship is much like creating a character
-         * You will need some assets for the ship parts
-         * You can add ship-exclusive cards and artifacts too */
-
-        /* Let's start with registering the ship parts, so we don't have to do it while making the ship proper
-         * You may notice these assets are copies of the vanilla parts. Don't worry, you can get wild with your own designs! */
-        /*var demoShipPartWing = helper.Content.Ships.RegisterPart("DemoPart.Wing", new PartConfiguration()
-        {
-            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/ships/demowing.png")).Sprite
-        });
-        var demoShipPartCannon = helper.Content.Ships.RegisterPart("DemoPart.Cannon", new PartConfiguration()
-        {
-            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/ships/democannon.png")).Sprite
-        });
-        var demoShipPartMissiles = helper.Content.Ships.RegisterPart("DemoPart.Missiles", new PartConfiguration()
-        {
-            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/ships/demomissiles.png")).Sprite
-        });
-        var demoShipPartCockpit = helper.Content.Ships.RegisterPart("DemoPart.Cockpit", new PartConfiguration()
-        {
-            Sprite = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/ships/democockpit.png")).Sprite
-        });
-        var demoShipSpriteChassis = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/ships/demochassis.png")).Sprite;
-
-        /* With the parts and sprites done, we can now create our Ship a bit more easily */
-        /*DemoMod_Ship = helper.Content.Ships.RegisterShip("DemoShip", new ShipConfiguration()
-        {
-            Ship = new StarterShip()
-            {
-                ship = new Ship()
-                {
-                    /* This is how much hull the ship will start a run with. We recommend matching hullMax */
-        /*hull = 12,
-        hullMax = 12,
-        shieldMaxBase = 4,
-        parts =
-        {
-            /* This is the order in which the ship parts will be arranged in-game, from left to right. Part1 -> Part2 -> Part3 */
-        /*new Part
-        {
-            type = PType.wing,
-            skin = demoShipPartWing.UniqueName
-        },
-        new Part
-        {
-            type = PType.cannon,
-            skin = demoShipPartCannon.UniqueName,
-            damageModifier = PDamMod.armor
-        },
-        new Part
-        {
-            type = PType.missiles,
-            skin = demoShipPartMissiles.UniqueName,
-            damageModifier = PDamMod.weak
-        },
-        new Part
-        {
-            type = PType.cockpit,
-            skin = demoShipPartCockpit.UniqueName
-        },
-        new Part
-        {
-            type = PType.wing,
-            skin = demoShipPartWing.UniqueName,
-            flip = true
-        }
-    }
-},
-
-/* These are cards and artifacts the ship will start a run with. The recommended card amount is 4, and the recommended artifact amount is 2 to 3 */
-        /*cards =
-        {
-            new CannonColorless(),
-            new DodgeColorless()
-            {
-                upgrade = Upgrade.A,
-            },
-            new DodgeColorless()
-            {
-                upgrade = Upgrade.B,
-            },
-            new BasicShieldColorless(),
-        },
-        artifacts =
-        {
-            new ShieldPrep(),
-            new DemoArtifactCounting()
-        }
-    },
-    ExclusiveArtifactTypes = new HashSet<Type>()
-    {
-        /* If you make some artifacts that you want only this ship to encounter in a run, here is where you place them */
-        /*typeof(DemoArtifactCounting)
-    },
-
-    UnderChassisSprite = demoShipSpriteChassis,
-    Name = AnyLocalizations.Bind(["ship", "DemoShip", "name"]).Localize,
-    Description = AnyLocalizations.Bind(["ship", "DemoShip", "description"]).Localize
-});
-
-/* 4. STATUSES
- * You might, now, with all this code behind our backs, start recognizing patterns in the way we can register stuff. */
-        /*AutododgeLeftNextTurn = helper.Content.Statuses.RegisterStatus("AutododgeLeftNextTurn", new()
-        {
-            Definition = new()
-            {
-                /* We provide the icon as a Sprite type, you can find it in the given file location 
-                icon = helper.Content.Sprites.RegisterSprite(package.PackageRoot.GetRelativeFile("assets/icons/autododgeLeftNextTurn.png")).Sprite,
-                /* We give it a color, this is the border color that surrounds the status icon & number in-game 
-                color = new("b500be"),
-                /* We define if it's isGood = true or isGood = false. This will dictate if the number will be either blue or red 
-                isGood = true
-            },
-            Name = AnyLocalizations.Bind(["status", "AutododgeLeftNextTurn", "name"]).Localize,
-            Description = AnyLocalizations.Bind(["status", "AutododgeLeftNextTurn", "description"]).Localize
-        });*/
-        /* Check this out in Features/AutododgeLeftNextTurn.cs */
         EarthStoneDeposit = helper.Content.Statuses.RegisterStatus("EarthStoneDeposit", new()
         {
             Definition = new()
@@ -559,6 +371,18 @@ public sealed class Elestrals : SimpleMod
         JesterApi?.RegisterCharacterFlag("midrow", Equilynx_Deck.Deck);
         JesterApi?.RegisterCharacterFlag("destroyPositive", Equilynx_Deck.Deck);
         JesterApi?.RegisterProvider(new EquilynxJesterProvider());
+
+        helper.Events.OnModLoadPhaseFinished += (_, phase) =>
+        {
+            if (phase != ModLoadPhase.AfterDbInit)
+                return;
+            DuoApis = new(helper);
+            if (!DuoApis.RegisterDuos)
+                return;
+
+            foreach (Type type in Equilynx_DuoArtifact_Types)
+                AccessTools.DeclaredMethod(type, nameof(IElestralsArtifact.Register))?.Invoke(null, [helper]);
+        };
     }
 
     public override object? GetApi(IModManifest requestingMod)
